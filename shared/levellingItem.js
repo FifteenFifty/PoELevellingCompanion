c77a6d591fbf951e04c1a992dd705b25ab435a83
FifteenFifty <matt.bissenden@gmail.com>
2020-05-25 16:31:55 +0100

    Merged Steven's Styling

--- shared/levellingItem.js
+++ shared/levellingItem.js
@@ -5,6 +5,8 @@ import { Progress } from "../data/progress.js";
 import FormattedText from "../shared/formattedText.js";
 import {globalStyles} from "../styles/global.js";
 
+import { WaypointLabel, RequiredLabel, OptionalLabel } from "../shared/verticalLabel.js";
+
 export default class LevellingItem extends React.Component {
 
   /**
@@ -107,15 +109,110 @@ export default class LevellingItem extends React.Component {
         <TouchableOpacity key={"Touchable" + item.id}
                           onPress={() => pressHandler(item.id)}>
           <View key={"ObjectiveContainer" + item.id}
-                style={globalStyles.item}>
-            { objectiveLine }
-            <View key={"RewardContainer" + item.id}
-                  style={globalStyles.info}>
-              { infoLine }
+                style={this.styles.container}>
+            <WaypointLabel active={item.hasWp} />
+            <View style={this.styles.centerBlock}>
+              { objectiveLine }
             </View>
+            { item.optional
+              ? <OptionalLabel secondaryText={"?"} />
+              : <RequiredLabel />
+            }
           </View>
         </TouchableOpacity>
       </View>
     );
   }
+
+  styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        minHeight: 50,
+        paddingTop: 2,
+        paddingBottom: 2,
+        borderBottomWidth: 2,
+        borderColor: "#4d4d4d"
+    },
+    label: {
+        display: "flex",
+        alignItems: "center",
+        width: 30,
+        justifyContent: "center",
+    },
+    labelTextLeft: {
+        fontSize: 20,
+        marginRight: 6,
+        width: 100,
+        textAlign: "center",
+        color: "white",
+        transform: [
+            {rotate: "-90deg"}
+        ]
+    },
+    labelTextRight: {
+        fontSize: 20,
+        marginLeft: 6,
+        width: 100,
+        textAlign: "center",
+        color: "white",
+        transform: [
+            {rotate: "90deg"}
+        ]
+    },
+    waypointLabelActive: {
+        backgroundColor: "#84a9e1"
+    },
+    waypointLabelInactive: {},
+    centerBlock: {
+        flexGrow: 1
+    },
+    requiredLabel:{
+        backgroundColor: "#aade87"
+    },
+
+  item: {
+    padding:10,
+    marginTop: 10,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 10
+  },
+  objective: {
+    flexDirection: "row"
+  },
+  info: {
+    flexDirection: "row"
+  },
+  infoItem: {
+    padding: 5
+  },
+  complete: {
+    textDecorationLine: "line-through",
+    opacity: .25
+  },
+  required: {
+    fontWeight: "bold"
+  },
+  boss: {
+    //TODO
+    color: "red"
+  },
+  area: {
+    //TODO
+    color: "blue"
+  },
+  quest: {
+    //TODO
+    color: "green"
+  },
+  waypoint: {
+    //TODO
+    color: "lightblue"
+  },
+  trial: {
+    //TODO
+    color: "teal"
+  }
+})
 }
---
+++ shared/verticalLabel.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function VerticalLabel({text, down, containerStyle}) {
+    return (
+        <View style={[styles.container,
+                      containerStyle,
+                      down ? styles.containerDown : styles.containerUp]}>
+            <Text style={[styles.text, down ? styles.textDown : styles.textUp]}>{text}</Text>
+        </View>
+    );
+}
+
+export function WaypointLabel({active}) {
+  console.log("WP active? " + active)
+    return (
+        <VerticalLabel text={ active ? "WP" : "" }
+                       down={false}
+                       containerStyle={active ? styles.waypointActiveContainer
+                                              : styles.waypointInactiveContainer} />
+    );
+}
+
+export function RequiredLabel() {
+    return (
+        <VerticalLabel text="Req"
+                       down={true}
+                       containerStyle={styles.requiredContainer} />
+    );
+}
+
+export function OptionalLabel({secondaryText}) {
+    return (
+        <View style={styles.optionalContainer}>
+            <VerticalLabel text={secondaryText}
+                           down={true}
+                           containerStyle={styles.optionalLabelContainer} />
+            <VerticalLabel text="Opt"
+                           down={true}
+                           containerStyle={styles.optionalLabelContainer} />
+        </View>
+    );
+}
+
+const textOffset = 6;
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        alignItems: "center",
+        width: 30,
+        justifyContent: "center",
+    },
+    containerDown: {
+        marginLeft: 2,
+    },
+    containerUp: {
+        marginRight: 2,
+    },
+    text: {
+        fontSize: 20,
+        textAlign: "center",
+        color: "white",
+    },
+    textDown: {
+        marginLeft: textOffset,
+        transform: [
+            {rotate: "90deg"}
+        ]
+    },
+    textUp: {
+        marginRight: textOffset,
+        transform: [
+            {rotate: "-90deg"}
+        ]
+    },
+    waypointActiveContainer: {
+        backgroundColor: "#84a9e1"
+    },
+    waypointInactiveContainer: {
+    },
+    requiredContainer: {
+        backgroundColor: "#aade87"
+    },
+    optionalContainer: {
+        flexDirection: "row"
+    },
+    optionalLabelContainer: {
+        backgroundColor: "#4d4d4d"
+    },
+})
